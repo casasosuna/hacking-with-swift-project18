f61dd1c2d2938df10a3bb482f4866af135f1cd60
enriquecasasjr <ecasasosuna@gmail.com>
2021-12-04 20:17:08 -0600

    Add files via upload

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Enrique Casas on 12/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Some message", terminator: " ")
+        
+        print(1, 2, 3, 4, 5, separator: "-")
+        
+       
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
